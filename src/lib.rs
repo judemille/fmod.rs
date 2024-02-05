9c4383cd01d7da09cc29ba30d1c6976aeb895455
Julia DeMille <me@jdemille.com>
2024-02-05 09:20:25 -0600

    REUSE compliance
    Signed-off-by: Julia DeMille <me@jdemille.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
 
+// SPDX-FileCopyrightText: 2024 Julia DeMille <me@jdemille.com>
+//
+// SPDX-License-Identifier: MPL-2.0
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
